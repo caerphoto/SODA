a67d753d71b9bf9ecc9ff0d416be861eb6b5a4e4
Andy <andy.farrell@phgroup.com>
2011-03-31 18:49:21 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soda::Application.config.secret_token = 'ea9509525e89e63e0524e82e1cfe52bb242c1246f1bd93aea5ee3b9b88bd2874995c6bb34fb1b5795871a171db538b33d9e3e0d54577f2b6cbcb501227802e75'
