8af8f44457627a0881da769a9af73a3e3c949631
Andy <andy.farrell@phgroup.com>
2011-04-08 08:49:43 +0100

    Rewrote sample text to be a basic Markdown guide, and added auto-zooming of preview.

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,8 @@ module Soda
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(underscore-min showdown-min http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js)
+    #config.action_view.javascript_expansions[:defaults] = %w(underscore-min showdown-min http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js)
+    config.action_view.javascript_expansions[:defaults] = %w(underscore-min showdown-min jquery.min)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,32 @@
-/*global $, Attacklab */
+/*global $, Attacklab, window */
 
 $(function () {
     var $input = $("#editor"),
         $preview = $("#preview"),
+        $toolbar = $("#toolbar"),
+
         showdown = new Attacklab.showdown.converter(),
-        prevText = "";
+        prevText = "",
+
+        pageWidth = 210, // assume A4 paper size for now
+
+        zoom = 1, PPI = 96, PPMM = PPI / 25.4,
+
+        // Functions:
+        pxToPt, ptToPx, pxToMm, ptToMm, mmToPx;
+
+    pxToPt = function (px) {
+        // 72 points per inch.
+        return px * zoom * 72 / PPI;
+    };
+
+    ptToPx = function (pt) {
+        return pt * zoom * PPI / 72;
+    };
+
+    mmToPx = function (mm) {
+        return mm * zoom * PPMM;
+    };
 
     $input.keyup(function () {
         var newText = $input.val();
@@ -17,6 +39,14 @@ $(function () {
         prevText = newText;
     });
 
-    $input.focus().keyup();
+    // Calculate font size based on preview 'page' width.
+    $(window).resize(function () {
+        // Assume A4 paper size for now (210mm wide).
+        zoom = $preview.innerWidth() / pageWidth / PPMM;
 
+        $preview.css({ fontSize: ptToPx(12), padding: mmToPx(20) });
+    });
+
+    $input.focus().keyup();
+    $(window).resize();
 });
