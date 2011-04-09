d692eb5c547035bb267891e7c168c24cdd616d29
Andy <andy.farrell@phgroup.com>
2011-04-09 19:36:28 +0100

    Changed scaling method to use CSS transform, as this results in a much more accurate preview with no 'jupming' as the text resizes.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,14 +2,17 @@
 
 $(function () {
     var $input = $("#editor"),
-        $preview = $("#preview"),
+        $previewScroller = $("#preview-scroller"),
+        $previewPages = $previewScroller.children(".preview-page"),
+        $pageSplitters = $previewScroller.children(".page-splitter"),
         $toolbar = $("#toolbar"),
         $zoomLevel = $("#zoom-level"),
 
         showdown = new Attacklab.showdown.converter(),
         prevText = "",
 
-        pageWidth = 210, // assume A4 paper size for now
+        PAGE_WIDTH = 210, // assume A4 paper size for now
+        PAGE_HEIGHT = 297,
 
         zoom = 1, PPI = 96, PPMM = PPI / 25.4,
 
@@ -35,7 +38,7 @@ $(function () {
             return;
         }
 
-        $preview.html(showdown.makeHtml(newText));
+        $previewPages.html(showdown.makeHtml(newText));
 
         prevText = newText;
     });
@@ -43,11 +46,22 @@ $(function () {
     // Calculate font size based on preview 'page' width.
     $(window).resize(function () {
         // Assume A4 paper size for now (210mm wide).
-        zoom = $preview.innerWidth() / pageWidth / PPMM;
+        zoom = ($previewScroller.width() - 60) / PAGE_WIDTH / PPMM;
+
+        $previewPages.css({
+            "-webkit-transform": "scale(" + zoom + ")",
+            "-moz-transform": "scale(" + zoom + ")",
+            "-o-transform": "scale(" + zoom + ")",
+            "-ms-transform": "scale(" + zoom + ")",
+            "transform": "scale(" + zoom + ")"
+        });
+
+        //$pageSplitters.css({
+            //width: mmToPx(PAGE_WIDTH),
+            //height: mmToPx(PAGE_HEIGHT)
+        //});
 
         $zoomLevel.text(Math.round(zoom * 100));
-
-        $preview.css({ fontSize: ptToPx(12), padding: mmToPx(20) });
     });
 
     $input.focus().keyup();
